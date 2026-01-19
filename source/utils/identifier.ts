96e0345825056e3282e230cbd1053b493346691a
Ted V. Kremer <tedvkremer@gmail.com>
2026-01-19 10:28:49 -0600

    Update identifier.ts

--- source/utils/identifier.ts
+++ source/utils/identifier.ts
@@ -31,9 +31,7 @@ export const id = (): ID => {
   bytes[8] = (bytes[8] & 0x3f) | 0x80
 
   const hex = Array.from(bytes, byte => byte.toString(16).padStart(2, '0'))
-  return `${hex.slice(0, 4).join('')}-${hex.slice(4, 6).join('')}-${hex.slice(6, 8).join('')}-${
-    hex.slice(8, 10).join('')
-  }-${hex.slice(10, 16).join('')}`
+  return `${hex.slice(0, 4).join('')}-${hex.slice(4, 6).join('')}-${hex.slice(6, 8).join('')}-${hex.slice(8, 10).join('')}-${hex.slice(10, 16).join('')}`
 }
 
 /** Validates if a string is a valid UUID v7. */
