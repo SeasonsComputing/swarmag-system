7415d172d891f11d18b6768a28c31a7eedd49f11
Ted V. Kremer <tedvkremer@gmail.com>
2026-05-08 10:26:39 -0500

    Style clean-up

--- source/ux/app-style-guide/app.tsx
+++ source/ux/app-style-guide/app.tsx
@@ -4,21 +4,19 @@
 
 import { onMount } from '@solid-js'
 import { render } from '@solid-js/web'
+import { StyleGuide } from './style-guide.tsx'
+
 import '@ux/common/assets/css/tokens.css'
 import '@ux/common/assets/css/base.css'
 import '@ux/common/assets/css/controls.css'
 import '@ux/common/assets/css/forms.css'
-import { StyleGuide } from './style-guide.tsx'
 import './style-guide.css'
 
 const root = document.getElementById('root')
 if (!root) throw new Error('Missing #root element')
 
 const App = () => {
-  onMount(() => {
-    document.body.style.opacity = '1'
-  })
-
+  onMount(() => document.body.style.opacity = '1')
   return <StyleGuide />
 }
 
